d2d45e773bb7ef709318924f9f81594f2481bded
Candarin <candarinfall@gmail.com>
2023-03-21 16:23:49 +1100

    Intox wip

--- Components/Intoxication/jornIntoxication.js
+++ Components/Intoxication/jornIntoxication.js
@@ -46,18 +46,113 @@ export async function onIntoxSavingThrow(event) {
         data-drink-strength=${selectedDrinkTypeStrength} 
         data-saving-throw-dc=${intoxSaveDC}
     */
-    
+    // Variables
+    let actorCurrentIntoxLevel = 0;
+
+
     console.log('Jorn | Intox Saving Throw Data: ');
     console.log(event);
 
     let actorId = event.target.dataset.actorId;
-
-    // let actorId = ($(this).data('actor-id'));
-
+    let rollDC = event.target.dataset.savingThrowDc;    
     let a = game.actors.get(actorId);
-    
     console.log('Jorn | actor: ' + actorId + ', ' + a.name);
 
-    a.rollAbilitySave("con");
+    // Get Actor data
+    // Check for current intox level flag
+    try {
+        tempFlag = await a.getFlag('JornForFoundryVTT', 'currentIntoxLevel');
+
+        // validate value
+        if (typeof tempFlag === 'undefined') {
+            // val is null
+            actorCurrentIntoxLevel = 0
+            await a.setFlag('JornForFoundryVTT', 'currentIntoxLevel', 0);
+            console.log('Jorn | actorCurrentIntoxLevel (created flag): ' + actorCurrentIntoxLevel);
+        } else {
+            // val is ok
+            actorCurrentIntoxLevel = tempFlag;
+            console.log('Jorn | actorCurrentIntoxLevel (found flag): ' + actorCurrentIntoxLevel);
+        }
+    } catch (error) {
+        // flag doesn't exist
+
+        // Create flag if it doesn't exist
+        actorCurrentIntoxLevel = 0;
+        await a.setFlag('JornForFoundryVTT', 'currentIntoxLevel', 0);
+        console.log('Jorn | actorCurrentIntoxLevel (error, created flag): ' + actorCurrentIntoxLevel);
+    }
+
+    // Call for saving throw
+    let rollResult = await a.rollAbilitySave("con");
+    console.log(rollResult);
+
+    // Compare Result to DC
+    if (rollResult >= rollDC) {
+        // Passed
+        // create message content
+        let messageContent = `<div class='dnd5e chat-card item-card'>`
+        messageContent += `<div class='card-content'>`
+        messageContent += `${token.actor.name} passes the save!`       
+        messageContent += `</div>`
+        messageContent += `</div>`
+
+        // create the message
+        if (messageContent !== '') {
+            let chatData = {
+                //user: game.user._id,
+                speaker: a,
+                content: messageContent
+            };
+           
+            ChatMessage.create(chatData, {});
+        };
+
+    } else {
+        // Failed
+
+        // How much did they fail by
+        let saveDif = rollDC - rollresult;
+        switch (saveDif) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                // code block
+                break;
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                // code block
+                break;
+            default:
+            // code block
+        }
+        
+
+        // Update actor
+
+        // Apply effect
+
+        
+        // create message content
+        let messageContent = `<div class='dnd5e chat-card item-card'>`
+        messageContent += `<div class='card-content'>`
+        messageContent += `${token.actor.name} failed the save!`
+        messageContent += `</div>`
+        messageContent += `</div>`
+
+        // create the message
+        if (messageContent !== '') {
+            let chatData = {
+                //user: game.user._id,
+                speaker: a,
+                content: messageContent
+            };
+
+            ChatMessage.create(chatData, {});
+        };
+    }
 
 } 
